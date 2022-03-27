876494068e8d1ee7c035030fe6a79ebcb40d6467
Rachel Hill <hillrachel187@gmail.com>
2022-03-27 14:59:32 -0600

    Make adjustments to recipe-repository class and tests

--- src/classes/RecipeRepository.js
+++ src/classes/RecipeRepository.js
@@ -1,18 +1,18 @@
 class RecipeRepository {
   constructor(recipeData) {
     this.recipeData = recipeData;
-  }
+  };
 
   filterTag(tag) {
     const filterRecipeByTag = this.recipeData.filter(recipe => recipe.tag.includes(tag))
     return filterRecipeByTag
-  }
+  };
 
   filterName(name) {
     const filterRecipeByName = this.recipeData.filter(recipe => recipe.name.includes(name))
     return filterRecipeByName
-  }
+  };
 
-}
+};
 
 export default RecipeRepository;
