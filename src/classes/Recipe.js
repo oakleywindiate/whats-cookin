93c7653ff29c49d5ad36c4bd89206a7f5be55469
Rachel Hill <hillrachel187@gmail.com>
2022-03-27 14:58:21 -0600

    Create recipe class

---
+++ src/classes/Recipe.js
@@ -0,0 +1,19 @@
+class Recipe {
+  constructor(recipe) {
+    this.recipe = recipe;
+  }
+
+  getIngredient(id) {
+
+  }
+  // input - we need to pass in ingredient array
+  // output - return array of ingredient names from ingredientData
+    // set up / how:
+      // since we have access to ingredientData by passing it in
+      // ingredient
+  // return the names of ingredients
+  // match the ids from recipe object to the ingredients data array to pull the name of ingredient needed
+
+};
+
+export default Recipe;
