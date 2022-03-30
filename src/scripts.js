8940abd1f97c5695182f67fa945241b78032e2b0
Oakley Windiate <oakley.windiate@gmail.com>
2022-03-29 18:14:25 -0600

    Create function and event listener for creating recipe list

--- src/scripts.js
+++ src/scripts.js
@@ -10,7 +10,7 @@ import Ingredient from './classes/Ingredient';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
 
-
+let recipeButtonList = document.querySelector('.recipe-list')
 
 console.log('Hello world');
 
@@ -19,7 +19,28 @@ let recipeList = new RecipeRepository(recipeData);
 let recipe1 = new Recipe(recipeList.recipeData[0])
 
 
+
 // console.log(recipeList.displayNames())
 // console.log(recipeList)
 // console.log(recipeList.filterName("Baked Stuffed Artichokes"))
 // console.log(recipeList.length)
+
+// view list of all Recipies
+// access recipe RecipeRepository
+// iterate through array
+// make a new button for each recipe
+
+
+
+
+const createRecipeList = () => {
+  const displayNames = recipeList.displayNames()
+  displayNames.forEach(name => {
+    console.log(name)
+    recipeButtonList.innerHTML += `<button class="recipe-list-button">${name}</button>`
+  })
+}
+
+// -------------- EVENT LISTENERS ----------------- //
+
+window.addEventListener('load', createRecipeList)
