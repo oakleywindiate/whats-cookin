247dc373dfa9a50909b832dec4da56460da7aaa4
Nikhila Balmoori <nbalmoori@gmail.com>
2022-04-03 19:02:43 -0600

    Fetch API data

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,29 @@
 // Your fetch requests will live here!
 
+let usersDataAPI
+let ingredientsDataAPI
+let recipesDataAPI
 
-console.log('I will be a fetch request!')
+const getFetchAll = () => {
+  usersDataAPI = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/users")
+    .then(response => response.json())
+  ingredientsDataAPI = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/ingredients")
+    .then(response => response.json())
+  recipesDataAPI = fetch("https://what-s-cookin-starter-kit.herokuapp.com/api/v1/recipes")
+    .then(response => response.json())
+  .catch((error) => {
+    return displayError(error)
+  })
+}
+
+const displayError = (error) => {
+  if (error.message === "Failed to fetch!") {
+    errorTag.innerText = "OPPS, SORRY! Something went wrong!";
+  } else {
+    errorTag.innerText = error.message;
+  }
+}
+
+// console.log(usersDataAPI)
+
+export {usersDataAPI, ingredientsDataAPI, recipesDataAPI, getFetchAll, displayError}
