501f6d66fd43d7e0709e15beb6d9544ea590cb2b
Nikhila Balmoori <nbalmoori@gmail.com>
2022-04-04 15:28:40 -0600

    Refactor for spacing and semi colons

--- src/classes/User.js
+++ src/classes/User.js
@@ -5,32 +5,32 @@ class User {
     this.userData = userData;
     this.favorites = [];
     this.recipesToCook = [];
-  };
+  }
 
   favoriteRecipe(id, recipeRepository) {
-    const recipeObject = recipeRepository.findRecipeById(id)
+    const recipeObject = recipeRepository.findRecipeById(id);
     if (!this.favorites.includes(recipeObject)) {
       this.favorites.push(recipeRepository.findRecipeById(id));
-    }
+    };
   }
 
   unfavoriteRecipe(id) {
     const unfavoriteIndex = this.favorites.findIndex(favoritedRecipe => {
-      return favoritedRecipe.recipe.id === id
-    })
+      return favoritedRecipe.recipe.id === id;
+    });
     this.favorites.splice(unfavoriteIndex, 1);
   }
 
   filterFavoriteTags(tag) {
     const filterFavoriteByTag = this.favorites.filter(favorite =>
-    favorite.recipe.tags.includes(tag))
-    return filterFavoriteByTag
-  };
+      favorite.recipe.tags.includes(tag));
+    return filterFavoriteByTag;
+  }
 
   filterFavoriteNames(name) {
-    const filterFavoriteRecipesByName = this.favorites.filter(favorite => favorite.recipe.name.includes(name))
-    return filterFavoriteRecipesByName
-  };
+    const filterFavoriteRecipesByName = this.favorites.filter(favorite => favorite.recipe.name.includes(name));
+    return filterFavoriteRecipesByName;
+  }
 
   addRecipesToCook(id, recipeRepository) {
     this.recipesToCook.push(recipeRepository.findRecipeById(id));
