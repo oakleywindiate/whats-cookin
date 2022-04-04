ee861ddfb308b63cd015241d17ff6e85b08e6dc7
Nikhila Balmoori <nbalmoori@gmail.com>
2022-04-04 15:28:50 -0600

    Refactor for spacing and semi colons

--- src/classes/UserRepository.js
+++ src/classes/UserRepository.js
@@ -4,16 +4,15 @@ class UserRepository {
   constructor(userData) {
     this.userData = userData;
     this.userObjects = this.createUsers();
-  };
+  }
 
   createUsers() {
     const mapUser = this.userData.map(user => {
-      const createClass = new User(user)
-      return createClass
+      const createClass = new User(user);
+      return createClass;
     });
-    return mapUser
-  };
-
+    return mapUser;
+  }
 };
 
 export default UserRepository;
