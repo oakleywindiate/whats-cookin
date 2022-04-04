eeef1a61e44c9fd6120172ac7c18b26691bbd79f
Nikhila Balmoori <nbalmoori@gmail.com>
2022-04-04 14:26:11 -0600

    Update favoriting recipe method to not duplicate recipes

--- src/classes/User.js
+++ src/classes/User.js
@@ -8,7 +8,10 @@ class User {
   };
 
   favoriteRecipe(id, recipeRepository) {
-    this.favorites.push(recipeRepository.findRecipeById(id));
+    const recipeObject = recipeRepository.findRecipeById(id)
+    if (!this.favorites.includes(recipeObject)) {
+      this.favorites.push(recipeRepository.findRecipeById(id));
+    }
   }
 
   unfavoriteRecipe(id) {
