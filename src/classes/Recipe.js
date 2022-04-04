65fa5a05bafb1ff1152f63f68b38addc5fe04769
Nikhila Balmoori <nbalmoori@gmail.com>
2022-04-04 15:28:14 -0600

    Refactor for spacing and semi colons

--- src/classes/Recipe.js
+++ src/classes/Recipe.js
@@ -5,19 +5,19 @@ class Recipe {
 
   getIngredient(ingredientsData) {
     const findIngredient = this.recipe.ingredients.map(ingredient => ingredientsData.getIngredientName(ingredient.id));
-    return findIngredient
-    };
+    return findIngredient;
+  }
 
   calculateCost(ingredientsData) {
     const getCost = this.recipe.ingredients.reduce((acc, ingredient) => {
-      acc += ingredient.quantity.amount * ingredientsData.getEstimatedCost(ingredient.id)
-      return acc
-    }, 0)
-    return getCost
-  };
+      acc += ingredient.quantity.amount * ingredientsData.getEstimatedCost(ingredient.id);
+      return acc;
+    }, 0);
+    return getCost;
+  }
 
   getDirections() {
-    return this.recipe.instructions
+    return this.recipe.instructions;
   }
 };
 
